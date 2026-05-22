b44bfdfce959316db3568f9f11b5317c42439385
Emilly Neves <emillyneves280@gmail.com>
2026-05-21 21:24:50 -0300

    definição  da cor do botão

--- Atividade01.html.js
+++ Atividade01.html.js
@@ -5,13 +5,23 @@ elementoForm.addEventListener("submit", (e) => e.preventDefault());
  elementoCor.addEventListener("input", (e) => {
     let corNova = e.target.value;
 
+    localStorage.setItem("primaryColor", corNova);
+
     document.documentElement.style.setProperty("--primary-color", corNova);
  });
 
  document.addEventListener("DOMContentLoaded", () => {
+     if (localStorage.getItem("primaryColor")){
+         elementoCor.value = localStorage.getItem("primaryColor");
+
+         document.documentElement.style.setProperty("--primary-color", elementoCor.value);
+     } else{
+
      const rootStyles = window.getComputedStyle(document.documentElement);
 
      const primaryColor = rootStyles.getPropertyValue("--primary-color").trim();
 
+
      elementoCor.value = primaryColor;
+     }
  });
